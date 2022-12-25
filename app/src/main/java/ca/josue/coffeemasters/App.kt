65279b19b9e2761483f38182a79a593c996afae7
Josue Lubaki <josuelubaki@gmail.com>
2022-12-25 08:42:42 -0500

    added Scaffold

---
+++ app/src/main/java/ca/josue/coffeemasters/App.kt
@@ -0,0 +1,50 @@
+package ca.josue.coffeemasters
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import ca.josue.coffeemasters.ui.theme.CoffeeMastersTheme
+
+@Preview
+@Composable
+fun App_Preview() {
+    CoffeeMastersTheme {
+        App()
+    }
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun App() {
+    Scaffold(
+        topBar = {
+            TopAppBar {
+                AppTitle()
+            }
+        },
+        bottomBar = {}
+    ){
+        OfferPage()
+    }
+}
+
+@Composable
+fun AppTitle() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo"
+        )
+    }
+}
